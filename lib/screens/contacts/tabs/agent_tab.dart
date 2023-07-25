1a90ed6958403a945100e38941f4387a71cd6708
fawad kaleem <fwdkaleem@gmail.com>
2023-07-26 02:45:12 +0500

    Show Contacts Value

--- lib/screens/contacts/tabs/agent_tab.dart
+++ lib/screens/contacts/tabs/agent_tab.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AgentTab extends StatefulWidget {
   const AgentTab({super.key});
@@ -10,6 +13,82 @@ class AgentTab extends StatefulWidget {
 class _AgentTabState extends State<AgentTab> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 1.4,
+      width: MediaQuery.of(context).size.width,
+      child: SingleChildScrollView(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("agents")
+              .where("id", isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            return SizedBox(
+              height: MediaQuery.of(context).size.height / 1.4,
+              child: ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  return StreamBuilder<Object>(
+                      stream: FirebaseFirestore.instance
+                          .collection("agents")
+                          .where("id",
+                              isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                          .snapshots(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (!snapshot.hasData) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                        if (snapshot.data!.docs.isEmpty) {
+                          return const Center(
+                            child: Text("No Agent Find"),
+                          );
+                        }
+                        final List<DocumentSnapshot> documents =
+                            snapshot.data!.docs;
+                        final Map<String, dynamic> data =
+                            documents[index].data() as Map<String, dynamic>;
+                        return Column(
+                          children: [
+                            ListTile(
+                              title: Text(
+                                data['contactName'],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              subtitle: Text(
+                                data['companyName'],
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              trailing: Container(
+                                padding: EdgeInsets.all(6.58),
+                                decoration:
+                                    BoxDecoration(color: Color(0xffDBDBDB)),
+                                child: SvgPicture.asset("assets/pencil.svg"),
+                              ),
+                            ),
+                            Divider()
+                          ],
+                        );
+                      });
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
