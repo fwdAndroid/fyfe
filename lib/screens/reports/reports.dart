0925187880e81307d57f0176deb2385614115c68
fawad kaleem <fwdkaleem@gmail.com>
2023-07-26 11:42:27 +0500

    Reports UI Ready with Data

--- lib/screens/reports/reports.dart
+++ lib/screens/reports/reports.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fyfe/widgets/custom_appbar.dart';
 import 'package:fyfe/widgets/custom_drawer.dart';
+import 'package:fyfe/widgets/drop.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Reports extends StatelessWidget {
@@ -15,10 +17,21 @@ class Reports extends StatelessWidget {
 
     return Scaffold(
       key: scaffoldkey,
-      drawer: CustomDrawer(
-        user: FirebaseAuth.instance.currentUser!.displayName,
-        userImage: FirebaseAuth.instance.currentUser!.photoURL,
-        signOut: GoogleSignIn().signOut,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        centerTitle: true,
+        backgroundColor: const Color(
+          0xff3AC1CD,
+        ),
+        elevation: 0,
+        title: const Text(
+          "Reports",
+          style: const TextStyle(
+              color: Colors.white,
+              fontFamily: "Nunito",
+              fontSize: 16,
+              fontWeight: FontWeight.w600),
+        ),
       ),
       body: SafeArea(
         child: Container(
@@ -41,27 +54,6 @@ class Reports extends StatelessWidget {
           padding: const EdgeInsets.fromLTRB(21.0, 21.0, 21.0, 21.0),
           child: Column(
             children: [
-              CustomAppbar(
-                title: "Reports",
-                trailing: InkWell(
-                  onTap: () {
-                    // Navigator.push(context,
-                    //     MaterialPageRoute(builder: (builder) => AddReport()));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 3),
-                    child: SvgPicture.asset(
-                      "assets/addProperty.svg",
-                    ),
-                  ),
-                ),
-                ontapLeading: () {
-                  scaffoldkey.currentState?.openDrawer();
-                },
-              ),
-              const SizedBox(
-                height: 18,
-              ),
               Row(
                 children: [
                   Expanded(
@@ -78,96 +70,84 @@ class Reports extends StatelessWidget {
                               fontWeight: FontWeight.w600)),
                     ),
                   ),
-                  // const SizedBox(
-                  //   width: 22,
-                  // ),
-                  // SvgPicture.asset("assets/filter.svg")
+                  const SizedBox(
+                    width: 22,
+                  ),
+                  SvgPicture.asset("assets/filter.svg")
                 ],
               ),
               SizedBox(
-                height: 20,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "All Properties",
-                        style: TextStyle(color: Colors.blueGrey, fontSize: 12),
-                        textAlign: TextAlign.center,
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          backgroundColor: Colors.white,
-                          fixedSize: Size(120, 50)),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "FInancial",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          backgroundColor: Colors.blueAccent,
-                          fixedSize: Size(120, 50)),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    ElevatedButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Admin",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          backgroundColor: Colors.blueAccent,
-                          fixedSize: Size(120, 50)),
-                    )
-                  ],
-                ),
+                height: 10,
               ),
               Container(
                 height: 400,
-                margin: EdgeInsets.only(top: 20),
-                child: ListView.builder(
-                    itemCount: 5,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: ListTile(
-                          trailing: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Image.asset(
-                              "assets/Group.png",
-                              height: 25,
-                            ),
-                          ),
-                          title: Text(
-                            "Date: 24/12/21",
-                            style: TextStyle(color: Colors.blue),
-                          ),
-                          subtitle: Text(
-                            "Type: Full Summary",
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ),
-                      );
+                margin: EdgeInsets.only(top: 10),
+                child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection("property")
+                        .where("uid",
+                            isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                        .snapshots(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (!snapshot.hasData) {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                      return ListView.builder(
+                          itemCount: snapshot.data!.docs.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final List<DocumentSnapshot> documents =
+                                snapshot.data!.docs;
+                            final Map<String, dynamic> data =
+                                documents[index].data() as Map<String, dynamic>;
+                            return Card(
+                              child: ListTile(
+                                trailing: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Image.asset(
+                                    "assets/Group.png",
+                                    height: 25,
+                                  ),
+                                ),
+                                title: Text(
+                                  data['propertyPurchaseDate'],
+                                  style: TextStyle(color: Colors.blue),
+                                ),
+                                subtitle: Text(
+                                  "Type: Full Summary",
+                                  style: TextStyle(color: Colors.grey),
+                                ),
+                              ),
+                            );
+                          });
                     }),
               ),
+              Text(
+                longText,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w300,
+                    fontSize: 10),
+                textAlign: TextAlign.justify,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Create  Report",
+                  style: TextStyle(color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                    backgroundColor: Color(0xff71C598),
+                    fixedSize: Size(350, 44)),
+              )
             ],
           ),
         ),
--- lib/widgets/drop.dart
+++ lib/widgets/drop.dart
@@ -49,3 +49,6 @@ agents(BuildContext context) {
     ),
   );
 }
+
+String longText =
+    "(i) Note: Reports are designed for quick access information shown on screen. For information that can be used in other applications, or to be shared, please use the Export button on each report.";
